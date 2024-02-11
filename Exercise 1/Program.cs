7b93c9f05539e376d5e6d6f3134ee4c17744c95c
GARCIA ALVAREZ SILVIA ANAHI <I23050337@monclova.tecnm.mx>
2024-02-10 21:19:31 -0600

    Agregar archivos de proyecto.

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,15 @@
+﻿// centimeters to inches conversion
+Console.WriteLine("Please enter the amount of centimeters to convert: ");
+string str  = Console.ReadLine();
+ 
+if (str.Length == 0)
+{
+	Console.WriteLine("Nothing was entered ");
+}
+else
+{
+	int centimeters = Convert.ToInt32(str);
+	double  multiplication = centimeters * 2.54;
+	Console.WriteLine("the result in inches is  " + multiplication);
+}
+
